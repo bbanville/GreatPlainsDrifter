cce22d450498d374714792c5fd3777b9b24b7f7b
Brendon <shadowsnpr561@gmail.com>
2020-10-29 14:10:34 -0700

    Added Updated Battle System

---
+++ Assets/Scripts/BattleSystem/BattleZoneTrigger.cs
@@ -0,0 +1,62 @@
+﻿
+using UnityEngine;
+using Opsive.UltimateCharacterController.Character;
+using Opsive.UltimateCharacterController.Game;
+using Opsive.UltimateCharacterController.Utility;
+using System;
+
+/// <summary>
+/// Notifies the BattleSystem when the character enters a trigger.
+/// </summary>
+public class BattleZoneTrigger : MonoBehaviour
+{
+    public event EventHandler OnPlayerTriggerEnter;
+    public event EventHandler OnPlayerTriggerExit;
+
+    private GameManager m_GameManager;
+
+    /// <summary>
+    /// Initialize the default values.
+    /// </summary>
+    private void Awake()
+    {
+        m_GameManager = GetComponentInParent<GameManager>();
+    }
+
+    /// <summary>
+    /// An object has entered the trigger.
+    /// </summary>
+    /// <param name="other">The object that entered the trigger.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        // A main character collider is required.
+        if (!MathUtility.InLayerMask(other.gameObject.layer, 1 << LayerManager.Character))
+        {
+            return;
+        }
+
+        OnPlayerTriggerEnter?.Invoke(this, EventArgs.Empty);
+        m_GameManager.EnteredTriggerZone(this, other.gameObject);
+    }
+
+    /// <summary>
+    /// An object has exited the trigger.
+    /// </summary>
+    /// <param name="other">The collider that exited the trigger.</param>
+    private void OnTriggerExit(Collider other)
+    {
+        // A main character collider is required.
+        if (!MathUtility.InLayerMask(other.gameObject.layer, 1 << LayerManager.Character))
+        {
+            return;
+        }
+
+        if (other.gameObject.GetCachedParentComponent<UltimateCharacterLocomotion>() == null || other.gameObject.GetCachedParentComponent<LocalLookSource>() != null)
+        {
+            return;
+        }
+
+        OnPlayerTriggerExit?.Invoke(this, EventArgs.Empty);
+        m_GameManager.ExitedTriggerZone(this);
+    }
+}
---
+++ Assets/Scripts/BattleSystem/Phase.cs
@@ -0,0 +1,123 @@
+﻿using BrendonBanville.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Phase : MonoBehaviour
+{
+    [Header("System Tracking")]
+    public int phaseID = 0;
+
+    [Header("Object Lists")]
+    [SerializeField] protected List<GameObject> enemyPhase;
+    [SerializeField] protected List<GameObject> objectPhase;
+
+    [ReadOnly] public bool phaseComplete = false;
+    [ReadOnly] private bool _PhaseSpawned = false;
+    [ReadOnly] private bool _PrevPhaseComplete = false;
+
+    /// <summary>
+    /// Awake is called once at the beginning of the scene
+    /// </summary>
+    protected void Awake()
+    {
+
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    protected void Update()
+    {
+        if (!phaseComplete)
+        {
+            if (phaseID == 0)
+            {
+                _PrevPhaseComplete = true;
+            }
+            else
+            {
+                _PrevPhaseComplete = PhaseManager.Instance.PrevPhaseComplete();
+            }
+
+            if (_PrevPhaseComplete)
+            {
+                if (!_PhaseSpawned)
+                {
+                    SpawnEnemies();
+                    SpawnObjects();
+                    _PhaseSpawned = true;
+                }
+
+                if (CheckEnemies() == true)
+                {
+                    phaseComplete = true;
+                    Invoke("DespawnEnemies", 2.0f);
+                }
+            }
+        }
+        else
+        {
+            PhaseManager.Instance.SpawnNextPhase();
+        }
+    }
+
+    /// <summary>
+    /// Spawns all enemies in the phase.
+    /// </summary>
+    public void SpawnEnemies()
+    {
+        foreach (GameObject enemy in enemyPhase)
+        {
+            enemy.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Despawns all enemies in the phase.
+    /// </summary>
+    protected void DespawnEnemies()
+    {
+        foreach (GameObject enemy in enemyPhase)
+        {
+            enemy.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Spawns all objects in the phase.
+    /// </summary>
+    public void SpawnObjects()
+    {
+        foreach (GameObject gameObject in objectPhase)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// Checks if all enemies are dead.
+    /// </summary>
+    protected bool CheckEnemies()
+    {
+        int alive = enemyPhase.Count;
+
+        foreach (GameObject enemy in enemyPhase)
+        {
+            if (PhaseManager.Instance.CheckAlive(enemy) == false)
+            {
+                alive--;
+            }
+        }
+
+        if (alive == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
