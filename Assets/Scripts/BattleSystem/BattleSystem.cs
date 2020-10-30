c8c4c99ce80229b7ac4158189c216ddbd0cffe2a
Brendon <shadowsnpr561@gmail.com>
2020-10-29 22:37:17 -0700

    BattleSystem Stuff

--- Assets/Scripts/BattleSystem/BattleSystem.cs
+++ Assets/Scripts/BattleSystem/BattleSystem.cs
@@ -6,146 +6,149 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BattleSystem : MonoBehaviour
+namespace Opsive.UltimateCharacterController.Demo
 {
-    public event EventHandler OnBattleStarted;
-    public event EventHandler OnBattleEnded;
-
-    private enum State
+    public class BattleSystem : MonoBehaviour
     {
-        WaitingToSpawn,
-        Active,
-        BattleOver,
-    }
+        public event EventHandler OnBattleStarted;
+        public event EventHandler OnBattleEnded;
 
-    /// <summary>
-    /// Represents a single Enemy Spawn Wave
-    /// </summary>
-    [System.Serializable]
-    public class Wave
-    {
-        public Transform enemySpawnContainer;
-        public DemoAgent[] enemyAgentArray;
-
-        public float time;
-        public bool alreadySpawned;
-    }
-
-    [SerializeField] private Wave[] waveArray;
-
-    [SerializeField] private BattleZoneTrigger startBattleTrigger;
-    //[SerializeField] private DoorAnims doorAnims;
-
-    private State state;
-    private List<DemoAgent> enemyAgentList;
-
-    private void Awake()
-    {
-        state = State.WaitingToSpawn;
-        enemyAgentList = new List<DemoAgent>();
-    }
-
-    private void Start()
-    {
-        //EventHandler.RegisterEvent<Vector3, Vector3, GameObject>("StartBattleTrigger_OnPlayerTriggerEnter", StartBattleTrigger_OnPlayerTriggerEnter);
-        startBattleTrigger.OnPlayerTriggerEnter += StartBattleTrigger_OnPlayerTriggerEnter;
-    }
-
-    private void StartBattleTrigger_OnPlayerTriggerEnter(object sender, System.EventArgs e)
-    {
-        StartBattle();
-        //EventHandler.UnregisterEvent<Vector3, Vector3, GameObject>("StartBattleTrigger_OnPlayerTriggerEnter", StartBattleTrigger_OnPlayerTriggerEnter);
-        startBattleTrigger.OnPlayerTriggerEnter -= StartBattleTrigger_OnPlayerTriggerEnter;
-    }
-
-    private void Update()
-    {
-        switch (state)
+        private enum State
         {
-            case State.Active:
-                foreach (Wave wave in waveArray)
-                {
-                    if (wave.alreadySpawned) continue; // Wave already spawned
-                    wave.time -= Time.deltaTime;
-                    if (wave.time <= 0f)
+            WaitingToSpawn,
+            Active,
+            BattleOver,
+        }
+
+        /// <summary>
+        /// Represents a single Enemy Spawn Wave
+        /// </summary>
+        [System.Serializable]
+        public class Wave
+        {
+            public Transform enemySpawnContainer;
+            public DemoAgent[] enemyAgentArray;
+
+            public float time;
+            public bool alreadySpawned;
+        }
+
+        [SerializeField] private Wave[] waveArray;
+
+        [SerializeField] private DemoZoneTrigger startBattleTrigger;
+        //[SerializeField] private DoorAnims doorAnims;
+
+        private State state;
+        private List<DemoAgent> enemyAgentList;
+
+        private void Awake()
+        {
+            state = State.WaitingToSpawn;
+            enemyAgentList = new List<DemoAgent>();
+        }
+
+        private void Start()
+        {
+            //EventHandler.RegisterEvent<Vector3, Vector3, GameObject>("StartBattleTrigger_OnPlayerTriggerEnter", StartBattleTrigger_OnPlayerTriggerEnter);
+            startBattleTrigger.OnPlayerTriggerEnter += StartBattleTrigger_OnPlayerTriggerEnter;
+        }
+
+        private void StartBattleTrigger_OnPlayerTriggerEnter(object sender, System.EventArgs e)
+        {
+            StartBattle();
+            //EventHandler.UnregisterEvent<Vector3, Vector3, GameObject>("StartBattleTrigger_OnPlayerTriggerEnter", StartBattleTrigger_OnPlayerTriggerEnter);
+            startBattleTrigger.OnPlayerTriggerEnter -= StartBattleTrigger_OnPlayerTriggerEnter;
+        }
+
+        private void Update()
+        {
+            switch (state)
+            {
+                case State.Active:
+                    foreach (Wave wave in waveArray)
                     {
-                        wave.alreadySpawned = true;
-                        SpawnWave(wave);
+                        if (wave.alreadySpawned) continue; // Wave already spawned
+                        wave.time -= Time.deltaTime;
+                        if (wave.time <= 0f)
+                        {
+                            wave.alreadySpawned = true;
+                            SpawnWave(wave);
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void SpawnWave(Wave wave)
+        {
+            List<DemoAgent> waveSpawnEnemyList = new List<DemoAgent>();
+            if (wave.enemySpawnContainer != null)
+            {
+                foreach (Transform transform in wave.enemySpawnContainer)
+                {
+                    DemoAgent enemySpawn = transform.GetComponent<DemoAgent>();
+                    if (enemySpawn != null)
+                    {
+                        waveSpawnEnemyList.Add(enemySpawn);
                     }
                 }
-                break;
-        }
-    }
+            }
 
-    private void SpawnWave(Wave wave)
-    {
-        List<DemoAgent> waveSpawnEnemyList = new List<DemoAgent>();
-        if (wave.enemySpawnContainer != null)
-        {
-            foreach (Transform transform in wave.enemySpawnContainer)
+            if (wave.enemyAgentArray != null)
             {
-                DemoAgent enemySpawn = transform.GetComponent<DemoAgent>();
-                if (enemySpawn != null)
+                waveSpawnEnemyList.AddRange(wave.enemyAgentArray);
+            }
+
+            foreach (DemoAgent enemyAgent in waveSpawnEnemyList)
+            {
+                enemyAgent.Spawn();
+                //enemyAgent.OnDead += DemoAgent_OnDead;
+                enemyAgentList.Add(enemyAgent);
+            }
+        }
+
+        private void StartBattle()
+        {
+            state = State.Active;
+
+            //if (doorAnims != null)
+            //{
+            //    doorAnims.SetColor(DoorAnims.ColorName.Red);
+            //    doorAnims.CloseDoor();
+            //}
+
+            OnBattleStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void EndBattle()
+        {
+            //if (doorAnims != null)
+            //{
+            //    doorAnims.SetColor(DoorAnims.ColorName.Green);
+            //    FunctionTimer.Create(doorAnims.OpenDoor, 1.5f);
+            //}
+
+            OnBattleEnded?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void EnemySpawn_OnDead(object sender, System.EventArgs e)
+        {
+            TestBattleOver();
+        }
+
+        private void TestBattleOver()
+        {
+            foreach (DemoAgent enemySpawn in enemyAgentList)
+            {
+                if (enemySpawn.IsAlive())
                 {
-                    waveSpawnEnemyList.Add(enemySpawn);
+                    // Still alive
+                    return;
                 }
             }
+
+            // All dead!
+            EndBattle();
         }
-
-        if (wave.enemyAgentArray != null)
-        {
-            waveSpawnEnemyList.AddRange(wave.enemyAgentArray);
-        }
-
-        foreach (DemoAgent enemyAgent in waveSpawnEnemyList)
-        {
-            enemyAgent.Spawn();
-            //enemyAgent.OnDead += DemoAgent_OnDead;
-            enemyAgentList.Add(enemyAgent);
-        }
-    }
-
-    private void StartBattle()
-    {
-        state = State.Active;
-
-        //if (doorAnims != null)
-        //{
-        //    doorAnims.SetColor(DoorAnims.ColorName.Red);
-        //    doorAnims.CloseDoor();
-        //}
-
-        OnBattleStarted?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void EndBattle()
-    {
-        //if (doorAnims != null)
-        //{
-        //    doorAnims.SetColor(DoorAnims.ColorName.Green);
-        //    FunctionTimer.Create(doorAnims.OpenDoor, 1.5f);
-        //}
-
-        OnBattleEnded?.Invoke(this, EventArgs.Empty);
-    }
-
-    private void EnemySpawn_OnDead(object sender, System.EventArgs e)
-    {
-        TestBattleOver();
-    }
-
-    private void TestBattleOver()
-    {
-        foreach (DemoAgent enemySpawn in enemyAgentList)
-        {
-            if (enemySpawn.IsAlive())
-            {
-                // Still alive
-                return;
-            }
-        }
-
-        // All dead!
-        EndBattle();
     }
 }
---
+++ Assets/Scripts/BattleSystem/DemoAgent.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using Opsive.UltimateCharacterController.Traits;
+using Opsive.UltimateCharacterController.Inventory;
+using CodeMonkey.Utils;
+
+namespace Opsive.UltimateCharacterController.Demo.BehaviorDesigner
+{
+    /// <summary>
+    /// Helper functions for the demo behavior tree.
+    /// </summary>
+    public class DemoAgent : MonoBehaviour
+    {
+        private Health m_Health;
+        private InventoryBase m_Inventory;
+        private ItemType m_ItemType;
+
+        // Expose the health and ammo via a Behavior Designer property mapping.
+        public float Health { get { return m_Health.HealthValue; } }
+        public float Ammo { get { return m_ItemType != null ? m_Inventory.GetItemTypeCount(m_ItemType) : float.MaxValue; } }
+
+        /// <summary>
+        /// Initialize the default values.
+        /// </summary>
+        private void Start()
+        {
+            m_Health = GetComponent<Health>();
+            m_Inventory = GetComponent<InventoryBase>();
+
+            // Find the ItemType.
+            var itemType = m_Inventory.DefaultLoadout[0].ItemType;
+            var item = m_Inventory.GetItem(0, itemType);
+            // If the first DefaultLoadout element is an item then the consumable ItemType should be retrieved.
+            if (item != null) {
+                var itemActions = item.ItemActions;
+                for (int i = 0; i < itemActions.Length; ++i) {
+                    var usableItem = itemActions[i] as Items.Actions.IUsableItem;
+                    if (usableItem != null) {
+                        m_ItemType = usableItem.GetConsumableItemType();
+                        break;
+                    }
+                }
+            } else {
+                m_ItemType = itemType;
+            }
+        }
+
+        /// <summary>
+        /// Spawns enemy.
+        /// </summary>
+        public void Spawn()
+        {
+            gameObject.SetActive(true);
+            transform.SetParent(null); // Go to root
+
+            //EnemyPathfindingMovement enemyPathfindingMovement = GetComponent<EnemyPathfindingMovement>();
+            //EnemyTargeting enemyTargeting = GetComponent<EnemyTargeting>();
+            //
+            //if (enemyPathfindingMovement != null) enemyPathfindingMovement.enabled = false;
+            //if (enemyTargeting != null) enemyTargeting.enabled = false;
+
+            //FunctionTimer.Create(() => {
+            //    if (enemyPathfindingMovement != null) enemyPathfindingMovement.enabled = true;
+            //    if (enemyTargeting != null) enemyTargeting.enabled = true;
+            //}, 1.5f);
+
+            //DissolveAnimate dissolveAnimate = GetComponent<DissolveAnimate>();
+            //if (dissolveAnimate != null)
+            //{
+            //    float dissolveTime = 2f;
+            //    dissolveAnimate.StartDissolve(1f, -1f / dissolveTime);
+            //}
+        }
+
+        /// <summary>
+        /// Checks if the character is alive.
+        /// </summary>
+        public bool IsAlive()
+        {
+            var health = GetComponent<Health>();
+
+            return health.IsAlive();
+        }
+    }
+}
